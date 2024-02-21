86d16e437c78f2c9b91c46fa47486ffb41b0fb84
Prawira Hadi Fitrajaya <fitrajayaprawira@gmail.com>
2024-02-21 13:51:14 +0700

    Initial Commit

---
+++ SimpleLoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLoginApp
+//
+//  Created by Prawira Hadi Fitrajaya on 21/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
